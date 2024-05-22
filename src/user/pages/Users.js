1083b37874ded42be4530120ddd563e8b204658b
Irina Volkhonskaya <irina.volkhonskaya@IMPRIVATA.com>
2024-05-22 17:56:48 +0200

    create a User component and Navigation

---
+++ src/user/pages/Users.js
@@ -0,0 +1,17 @@
+import React from "react";
+import UsersList from "../components/UsersList";
+import NewPlace from "../../places/pages/`/NewPlace";
+
+const Users = () => {
+  const USERS = [
+    {
+      id: "u1",
+      image: "https://static.toiimg.com/photo/75503656.cms",
+      name: "John Doe",
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
+};
+
+export default Users;
